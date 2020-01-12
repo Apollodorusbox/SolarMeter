1f46f8be826ee59c5536e45b66c3be87d725b8d7
Apollodorusbox <59804310+Apollodorusbox@users.noreply.github.com>
2020-01-13 02:35:07 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'eqJUIhKe5fG2JGVL20rC6QoPZpTF7kWbasKxKBpFBAA+xI3qs6JxrLx574kSa/VTage4z0mmTu6qokOt/bDlzm6ihIr3gSltvoI12E3pZrWO6jCjNoMHktKMQCRA8xiRu7cSb09C0W5oCV8pfmgDEwdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'U73de4991a95b9792e9785193741ace0c',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
