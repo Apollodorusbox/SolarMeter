d061eb4af77ac61b9b72f834d7a0066b52d6222d
Apollodorusbox <59804310+Apollodorusbox@users.noreply.github.com>
2020-01-13 02:32:47 +0700

    Update pub.php

--- pub.php
+++ pub.php
@@ -1,8 +1,8 @@
  <?php
  function pubMqtt($topic,$msg){
-       $APPID= "numpapicklinebot/"; //enter your appid
-     $KEY = "mJ7K4MfteC7p0dW"; //enter your key
-    $SECRET = "pp4gzMhCvJIqlxc66hKEvk46m"; //enter your secret
+       $APPID= "LineBotSolar/"; //enter your appid
+     $KEY = "JsnvWfjciNSmJDH"; //enter your key
+    $SECRET = "RTtlDkGoxEiQp1qhDgI87JrkL"; //enter your secret
     $Topic = "$topic"; 
       put("https://api.netpie.io/microgear/".$APPID.$Topic."?retain&auth=".$KEY.":".$SECRET,$msg);
  
